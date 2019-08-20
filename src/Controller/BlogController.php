10f51acbcbe0887cfcc0df5c53b68f3040be8350
silverquent <dasilva.quentin33@gmail.com>
2019-08-20 15:46:39 +0200

    New

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BlogController extends AbstractController
+{
+
+    /**
+     * @Route("/blog", name="blog_index")
+     */
+
+    public function index()
+    {
+        return $this->render('/index.html.twig', ['owner' => 'Quentin']);
+    }
+}
